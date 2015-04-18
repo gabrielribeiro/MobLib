881595ee2c21aff5eb2ded8a774b4ddaf31ea6a6
Guilherme Titschkoski <guilherme.ti@outlook.com>
2015-04-18 16:18:22 -0300

    Validation for CPPJ

--- Extensions/StringExtensions.cs
+++ Extensions/StringExtensions.cs
@@ -90,5 +90,73 @@ namespace MobLib.Extensions
 
             return cpf.EndsWith(digito);
         }
+
+
+        /// <summary>
+        /// Returns is the string is a valid cnpj or not
+        /// </summary>
+        /// <param name="cnpj"></param>
+        /// <returns></returns>
+        public static bool IsValidCnpj(this string cnpj)
+        {
+            int[] multiplicador1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            int soma;
+            int resto;
+            string digito;
+            string tempCnpj;
+
+            cnpj = cnpj.Trim();
+            cnpj = cnpj.Replace(".", "").Replace("-", "").Replace("/", "");
+
+            if (cnpj.Length != 14)
+                return false;
+
+            tempCnpj = cnpj.Substring(0, 12);
+            soma = 0;
+
+            for (int i = 0; i < 12; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador1[i];
+
+            resto = (soma % 11);
+
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            digito = resto.ToString();
+            tempCnpj = tempCnpj + digito;
+            soma = 0;
+
+            for (int i = 0; i < 13; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador2[i];
+
+            resto = (soma % 11);
+
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            digito = digito + resto.ToString();
+
+            return cnpj.EndsWith(digito);
+
+
+        }
+
+
+
+        public static bool IsValidCpfCnpj(this string cpfCnpj)
+        {
+            if (Regex.IsMatch(cpfCnpj, @"([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})"))
+            {
+                return cpfCnpj.IsValidCpf() || cpfCnpj.IsValidCnpj();                
+            }
+            return false;
+
+        }
     }
 }
