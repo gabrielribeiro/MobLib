b698252a3339aa102cfee34b2da568e95d7ea510
Guilherme Titschkoski <tits@balaodainformatica.com.br>
2015-05-27 16:40:29 -0300

    Comment

--- Extensions/DateTimeExtensions.cs
+++ Extensions/DateTimeExtensions.cs
@@ -4,6 +4,14 @@ namespace MobLib.Extensions
 {
     public static class DateTimeExtensions
     {
+        /// <summary>
+        /// Converts a T object to datetime
+        /// </summary>
+        /// <typeparam name="T">Type of the object to be converted</typeparam>
+        /// <param name="element">the object to be converted</param>
+        /// <returns>a valid datetime</returns>
+        /// <exception cref="System.FormatException"/>
+        /// <exception cref="System.InvalidCastException"/>
         public static DateTime ToDateTime<T>(this T element)
         {
             return Convert.ToDateTime(element);
